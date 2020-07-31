2f734cad7b5a542cf5cbc9a2f1c7c3c6692dcc9e
Liu <Liu.Bin@torontorehab.on.ca>
2020-07-31 18:03:52 -0400

    controller has been showed

--- Assets/HandPresence.cs
+++ Assets/HandPresence.cs
@@ -8,25 +8,41 @@ using Debug = UnityEngine.Debug;
 public class HandPresence : MonoBehaviour
 {
     // Start is called before the first frame update
-   
-    private InputDevices targetDevice;
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    //public GameObject handModelPrefab;
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    //private GameObject spawnedHandModel;
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>();
         //InputDevices.GetDevices(devices);
-        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics,devices);
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
-
-        Debug.Log("Hello: " + gameObject.name);
-
+        //Debug.Log("Hello: " + gameObject.name);
+        /*
         foreach (var item in devices)
         {
             Debug.Log(item.name + item.characteristics);
         }
+        */
         if(devices.Count > 0)
         {
             targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controllerPrefebs=>controllerPrefebs.name == targetDevice.name);
+            if(prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);                
+            }
+            else
+            {
+                Debug.LogError("Did not find controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            //spawnedHandModel = Instantiate(handModelPrefab, transform);
         }
         
 
@@ -36,5 +52,24 @@ public class HandPresence : MonoBehaviour
     void Update()
     {
         
+        if(targetDevice.TryGetFeatureValue(CommonUsages.primaryButton,out bool primaryButtonValue) && primaryButtonValue)
+            Debug.Log("Primary Button has been pressed");
+
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger,out float triggerValue) && triggerValue>0.1f)
+            Debug.Log("Trigger has been pressed" + triggerValue);
+
+        if(targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis,out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+            Debug.Log("Primary2DAxis has been pressed" + primary2DAxisValue);
+        /*
+        if(showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);      
+        }*/
     }
 }
